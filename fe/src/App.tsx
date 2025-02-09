d1132b004d9796d92a70f9debcfa69b40cc55a66
Sang <75106540+sang1833@users.noreply.github.com>
2025-02-09 13:44:07 +0700

    add hide search button for mobile

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -29,17 +29,11 @@ function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedTown, setSelectedTown] = useState<string>("Tất cả");
 
-  // const handleSelectStreetOnMap = (street: IStreetRoute | null) => {
-  //   const streetRoute = StreetInfoToIStreetRoute(street as StreetInfoType);
-  //   setSelectedStreet(streetRoute);
-  //   navigate(`/street/${street?.id}`);
-  //   if (street) {
-  //     setMapState({
-  //       center: street.route.coordinates[0],
-  //       zoom: 15
-  //     });
-  //   }
-  // };
+  const [isStreetSearchVisible, setIsStreetSearchVisible] = useState(true);
+
+  const toggleStreetSearchVisibility = () => {
+    setIsStreetSearchVisible(!isStreetSearchVisible);
+  };
 
   useEffect(() => {
     if (currentStreet !== null) {
@@ -82,19 +76,44 @@ function App() {
   return (
     <div className="flex flex-col h-screen">
       {/* Header */}
-      <header className="bg-[#145da0] text-white p-3">
+      <header className="bg-[#145da0] text-white p-3 static">
+        {/* Hide StreetSearch button */}
+        <button
+          className="absolute float-left sm:hidden p-1"
+          onClick={toggleStreetSearchVisibility}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className={`lucide lucide-circle-chevron-down transition-transform duration-300 ${
+              isStreetSearchVisible ? "" : "-rotate-90"
+            }`}
+          >
+            <circle cx="12" cy="12" r="10" />
+            <path d="m16 10-4 4-4-4" />
+          </svg>
+        </button>
         <div className="container mx-auto flex flex-col sm:flex-row items-center justify-between space-y-2 sm:space-y-0">
           <h1 className="text-2xl font-bold flex items-center">
             <img src={AppLogo} alt="Logo" className="w-8 h-8 mr-2" />
             <span className="hidden sm:inline">Bản đồ Bến Cát</span>
           </h1>
-          <StreetSearch
-            onSelectStreet={handleSelectStreet}
-            searchTerm={searchTerm}
-            setSearchTerm={setSearchTerm}
-            townFilter={selectedTown}
-            setTownFilter={setSelectedTown}
-          />
+          {isStreetSearchVisible && (
+            <StreetSearch
+              onSelectStreet={handleSelectStreet}
+              searchTerm={searchTerm}
+              setSearchTerm={setSearchTerm}
+              townFilter={selectedTown}
+              setTownFilter={setSelectedTown}
+            />
+          )}
         </div>
       </header>
       {/* Main */}
